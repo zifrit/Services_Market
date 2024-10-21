0ff971c75d12d95c2f6966a8751cd32d672bc8a5
zifrit <zifrit@list.ru>
2024-10-21 13:04:54 +0300

    added logging settings, configured the launch bot

--- core/settings.py
+++ core/settings.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+
+
 class Postgres(BaseSettings):
     DB_HOST: str = "localhost"
     DB_PORT: int = "5432"
@@ -15,4 +19,6 @@ class Postgres(BaseSettings):
     def database_url_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+
 db_settings = Postgres()
+bot_settings = Settings()
---
+++ run_me.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+import logging.config
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from core.settings import bot_settings
+from core.logger import LOGGING
+from handlers import (
+    start,
+)
+
+commands = [
+    types.BotCommand(command="start", description="запуск бота"),
+]
+
+
+async def main():
+    bot = Bot(
+        token=bot_settings.BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_routers(
+        start.router,
+    )
+    await bot.set_my_commands(commands=commands)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.config.dictConfig(LOGGING)
+    asyncio.run(main())

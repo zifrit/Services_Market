0f56a38d12ca1f8892bbd07f9054b96bb760534d
zifrit <zifrit@list.ru>
2024-10-22 19:01:37 +0300

    added vpn, price, association_vpn_price table

--- models/vpn.py
+++ models/vpn.py
@@ -2,7 +2,7 @@ import typing
 
 from models.base import IdCUDMixin
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import String, BigInteger, ForeignKey
+from sqlalchemy import String, BigInteger, ForeignKey, UniqueConstraint
 
 if typing.TYPE_CHECKING:
     from models.user import User
@@ -14,3 +14,35 @@ class UserVPN(IdCUDMixin):
     country: Mapped[str] = mapped_column(String(255))
     tg_user_id: Mapped[int] = mapped_column(ForeignKey("users.tg_id"))
     tg_user: Mapped["User"] = relationship(back_populates="user_vpn")
+
+
+class VPN(IdCUDMixin):
+    __tablename__ = "vpn"
+    country_view_text: Mapped[str] = mapped_column(String(255))
+    country: Mapped[str] = mapped_column(String(255))
+    price: Mapped[list["Price"]] = relationship(
+        secondary="association_vpn_price",
+        back_populates="vpn",
+    )
+
+
+class Price(IdCUDMixin):
+    __tablename__ = "price"
+    price_view_text: Mapped[str] = mapped_column(String(255))
+    price: Mapped[str] = mapped_column(String(255))
+    vpn: Mapped[list["VPN"]] = relationship(
+        secondary="association_vpn_price",
+        back_populates="price",
+    )
+
+
+class AssociationVPNPrice(IdCUDMixin):
+    __tablename__ = "association_vpn_price"
+    __table_args__ = (
+        UniqueConstraint("vpn_id", "price_id", name="idx_unique_order_product"),
+    )
+    vpn_id: Mapped[int] = mapped_column(ForeignKey("vpn.id"))
+    price_id: Mapped[int] = mapped_column(ForeignKey("price.id"))
+
+    vpn: Mapped["VPN"] = relationship(back_populates="price")
+    price: Mapped["Price"] = relationship(back_populates="vpn")

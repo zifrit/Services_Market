ebeb897831b443beabf463b56af436373d68e4eb
zifrit <zifrit@list.ru>
2024-10-24 20:38:38 +0300

    in price table added enum term field, str key_price field, int quantity field, fixed type price str > int

--- models/vpn.py
+++ models/vpn.py
@@ -1,8 +1,9 @@
 import typing
-
+import enum
 from models.base import IdCUDMixin
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import String, BigInteger, ForeignKey, UniqueConstraint
+from sqlalchemy import String, ForeignKey, UniqueConstraint, Integer
+from sqlalchemy.dialects.postgresql import ENUM as PGEnum
 
 if typing.TYPE_CHECKING:
     from models.user import User
@@ -32,10 +33,24 @@ class VPN(IdCUDMixin):
     )
 
 
+class Term(enum.Enum):
+    day = "day"
+    month = "month"
+    year = "year"
+
+
 class Price(IdCUDMixin):
     __tablename__ = "price"
     price_view_text: Mapped[str] = mapped_column(String(255))
-    price: Mapped[str] = mapped_column(String(255))
+    price: Mapped[int]
+    quantity: Mapped[int]
+    term: Mapped[Term] = mapped_column(
+        PGEnum(Term, name="price_term"),
+        comment="Вид времени",
+    )
+    key_price: Mapped[str] = mapped_column(
+        String(255), comment="Ключ для цены", unique=True
+    )
     vpn: Mapped[list["VPN"]] = relationship(
         secondary="association_vpn_price",
         back_populates="price",

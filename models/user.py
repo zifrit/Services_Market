ef136def25b722acdb9d755a8db327e74a2eb8e8
zifrit <zifrit@list.ru>
2024-10-24 12:48:31 +0300

    added view for users

--- models/user.py
+++ models/user.py
@@ -13,13 +13,21 @@ class User(IdCUDMixin):
     username: Mapped[str] = mapped_column(String(255))
     tg_id: Mapped[int] = mapped_column(BigInteger, unique=True)
     user_vpn: Mapped[list["UserVPN"]] = relationship(back_populates="tg_user")
+    # Рефералы, которых пригласил данный пользователь
     referred_users: Mapped[list["Referral"]] = relationship(
         "Referral",
         back_populates="referrer",
         foreign_keys="[Referral.referrer_id]",
     )
+    # Пользователь, который пригласил текущего пользователя
     invited_by: Mapped[list["Referral"]] = relationship(
         "Referral",
         back_populates="referred_user",
         foreign_keys="[Referral.referred_user_id]",
     )
+
+    def __str__(self) -> str:
+        return f"{self.tg_id} {self.username}"
+
+    def __repr__(self) -> str:
+        return str(self)

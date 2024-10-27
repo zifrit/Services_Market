b6cdbbe3e06bb5f29182c0a1e7089ecf79accf40
zifrit <zifrit@list.ru>
2024-10-27 18:58:28 +0300

    added view statistics user referral

--- crud/referral.py
+++ crud/referral.py
@@ -1,4 +1,6 @@
-from sqlalchemy import select
+from datetime import date, datetime, timedelta
+
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import User
@@ -11,8 +13,8 @@ async def create_referral(
 ):
     referrer_user = await get_user(session, tg_id=referrer_tg_id)
     referred_user = await get_user(session, tg_id=referred_tg_id)
-    print(referrer_user)
-    print(referred_user)
+    print(referred_tg_id)
+    print(referrer_tg_id)
     is_referral = await session.scalar(
         select(Referral).where(Referral.referred_user_id == referred_user.id)
     )
@@ -25,3 +27,23 @@ async def create_referral(
         await session.commit()
         return True
     return False
+
+
+async def get_referral_statics(session: AsyncSession, tg_id: int) -> dict[str, int]:
+    user = await get_user(session, tg_id=tg_id)
+    count_referral = await session.scalar(
+        select(func.count(Referral.id)).where(Referral.referred_user_id == user.id)
+    )
+    today = date.today()
+    start_of_day = datetime.combine(today, datetime.min.time())
+    end_of_day = start_of_day + timedelta(days=1)
+    count_referral_today = await session.scalar(
+        select(func.count(Referral.id)).where(
+            Referral.created_at.between(start_of_day, end_of_day),
+            Referral.referred_user_id == user.id,
+        )
+    )
+    return {
+        "count_referral": count_referral,
+        "count_referral_today": count_referral_today,
+    }
--- handlers/referral.py
+++ handlers/referral.py
@@ -6,6 +6,7 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.utils.deep_linking import create_start_link
 import logging
 from core.db_connections import db_helper
+from crud import referral as crud_referral
 
 
 loger = logging.getLogger(__name__)
@@ -17,8 +18,19 @@ router = Router()
 @router.callback_query(F.data == "partners")
 async def referral(call: CallbackQuery):
     link = await create_start_link(call.bot, f"{call.from_user.id}", encode=True)
+    async with db_helper.session_factory() as session:
+        statistics = await crud_referral.get_referral_statics(
+            session, call.from_user.id
+        )
     await call.message.edit_text(
-        f"`{link}`",
+        f"""
+Реферальная ссылка: `{link}`
+
+Статистика📊
+Рефералов за все время: {statistics['count_referral']}
+Рефералов за сегодня: {statistics['count_referral_today']}
+Активные рефералы:
+            """,
         reply_markup=back("🔙Назад", "back_to_start_menu"),
         parse_mode=ParseMode.MARKDOWN,
     )
